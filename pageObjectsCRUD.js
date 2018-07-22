0a652086e0e9243c590cec8abddf9eb8dbdf437f
Dave Mason <onthebay.dave@gmail.com>
2018-07-22 12:46:13 -0300

    Page will now display rows based on Search Term dropdown selection

--- pageObjectsCRUD.js
+++ pageObjectsCRUD.js
@@ -1,3 +1,133 @@
+
+function onclickDropdowns(element) {	//comes here for when an item in the dropdowns is clicked 
+	var $tablebody = $('#maintablebody');	
+	var liID = element.id;
+	var liText = document.getElementById(liID).innerHTML;	//Current text in <li>
+
+
+	alert("element click ID -- " + liID + " clicked element text -- " + liText);
+	switch(element.id.slice(0,5)) {
+		case 'searc':
+			var searchTerm = liText;	//current utility pageObj search term
+			var objName = "searchtem";	//used in success: switch
+			var forObject = "searchTermRows";	//get pageObject rows have selected SearchTerm
+			var sqlCommand = "";
+			console.log("filterItem" + " li ID -- " + liID);
+			break;
+	}
+	var postData = {
+		"forObject":forObject,	//used in switch() to customize code and function called in pageObjects.php
+		"sqlCommand":sqlCommand,	//used in creating SQL for data search
+		"pageObject":searchTerm,
+		"fieldname":"",
+		"clickedData":liID,
+		};
+	var dataString = JSON.stringify(postData);	//convert dataString string to JSON
+	$.ajax({
+	    url:'pageObjectsCRUD.php',
+	    type:'POST',
+	    data: {postOBJ: dataString},
+		success:function(returnData) {
+			console.log("returnData onclickDropdowns -- " + returnData + " returnData length -- " + returnData.length);
+			if (returnData.length <= 1) {			
+				var obj = $.parseJSON(returnData);
+				var myObj = JSON.parse(returnData.substring(1, returnData.length-1));
+			}
+			/*alert("In onclickDropdowns returnData length -- " + obj.length +
+			 "\n obj.status -- " + obj.status +
+			 "\n myObj.lenght -- " + myObj.length + 
+			 "\n myObj.status -- " + myObj.status);*/
+			//var newStr = returnData.substring(1, returnData .length-1);
+			//console.log("returnData -- " + myObj.status); //this breaks code when myObj.length = 1 !!
+//			if (myObj.status == 'Success') {				
+				//console.log("returnData -- " + returnData);				
+				//alert("Success with Ajax onclickDropdowns()) -- " + myObj.info);				
+				switch(objName) {
+					case "searchtem":
+						$tablebody.empty();
+						$.each(returnData, function(i, resultitem){
+					       	var tablerowtemplate = "<tr>" +
+					       			"<td>" + resultitem.ID + "</td>" +
+									"<td>" + resultitem.SearchTerm + "</td>" +
+									"<td>" + resultitem.orderItems + "</td>" +
+									"<td>" + resultitem.itemDisplay + "</td>" +
+									"<td>" + resultitem.itemSQL + "</td>" +
+									"<td>" + resultitem.ModifiedDate + "</td>" +
+									"<td>" +
+									"<a href='read.php?ID=" + resultitem.ID + "' title='View Record' data-toggle='tooltip'>" +
+										"<span class='fa fa-eye'></span></a>" +
+									"<a href='update.php?ID=" + resultitem.ID + "' title='Update Record' data-toggle='tooltip'>" +
+										"<span class='fa fa-pencil'></span></a>" +
+									"<a href='delete.php?ID=" + resultitem.ID + "' title='Delete Record' data-toggle='tooltip'>" +
+										"<span class='fa fa-trash'></span></a>" +
+									"</td>" +
+									"</tr>";
+							//alert("table row --" + tablerowtemplate);
+							$tablebody.append(tablerowtemplate);
+						});
+						break;
+				}
+//			} else {
+//				alert("Failed Ajax sortfield -- " + returnData);
+//			}
+		},
+        error: function() {
+        	alert('Error on sortby or filterby item clicked.');
+        }
+    //console.log("Out of switch");
+    });	
+}
+
+function pageObjectsList(searchTerm, forObject, elementID) {	//Get row data from pageObjects table where = searchTerm
+	var postData = {
+		"forObject":forObject,	//forObject used in pageObjects.php by switch case for custom code
+		"sqlCommand":"",
+		"pageObject":searchTerm,	//used to get row items
+		"fieldname":"",
+		"clickedData":"",
+	};
+	//alert("in pageObjectsList \n searchTerm -- " + searchTerm + "\n forObject -- " + forObject + "\n elementID -- " + elementID);
+	var dataString = JSON.stringify(postData);
+    $.ajax({
+        url:'pageObjectsCRUD.php',
+        type:'POST',
+        data: {postOBJ: dataString},
+        success:function(returnData) {
+			console.log("returnData -- " + returnData);			
+			var myOBJ = document.getElementById(elementID);
+			//alert("element myOBJ.id  -- " +  myOBJ.id + " elementID -- " + elementID);
+			var objIDname = "searchterm";
+			switch(forObject) {
+				case "searchTermDropdown":
+					var objIDname = "searchTerm";
+					break;
+
+			};
+			//alert("in pageObjectsList: idname -- " + objIDname);
+			$('#' + myOBJ.id).empty();
+			$.each(returnData, function(i, resultitem){
+				$('#' + myOBJ.id).append("<li id='" + objIDname + resultitem.ID + "' class='showitem'>" + resultitem.SearchTerm + "</li>");
+			});				
+        },
+        error: function() {
+        	alert('In pageObjectsList().  Error with getting data from pageObjects.php');
+        }
+	});
+};
+
+$(document).ready(function (){	// dropdowns clicked.
+	
+	$("#searchTermDropdown").on('click',function(){	//When SortBy dropdown data <li> is clicked
+		var element = event.target;
+		alert("Object in sortDropdown has been clicked -- " + element.id);		
+		onclickDropdowns(element)	//function changes element text and gets SQL for <li> choice
+	});
+
+
+
+	
+});
+
 $(document).ready(function(){	//Code to run when page finishes loading
 
 	//javascript session storage
@@ -9,5 +139,8 @@ $(document).ready(function(){	//Code to run when page finishes loading
 		sessionStorage.setItem("sessionStorageInit", "TRUE");
 
 	}
+	//pageObjectsList(searchTerm, forObject, $elementID)
+	pageObjectsList("selfUnique", 'searchTermDropdown', 'searchTermDropdown');	//Fill in <li> values for utilities dropdown
+
 
 });
---
+++ pageObjectsCRUD.php
@@ -0,0 +1,130 @@
+<?php
+
+	//Functions
+	
+	//function pass SearchTerm of pageObjects in $objectSearch
+	// return returnStatus contains the rows of data from search
+	function getDISTINCTSearchTerms() {
+		// Include config file
+	    require_once 'config.php';
+
+//SELECT DISTINCT `SearchTerm` FROM `pageObjects` ORDER BY `SearchTerm` LIMIT 0, 10
+    
+		$select = "SELECT DISTINCT `SearchTerm` ";
+		$from = "FROM `pageObjects` ";
+		$where = "";
+		$orderby = "ORDER BY `SearchTerm` ";
+		$limit = "LIMIT 0, 10";
+
+		$strSQL = $select . $from . $where . $orderby . $limit;				
+		//echo $strSQL;
+
+	    $result = $mysqli->query($strSQL);
+
+	    $fieldDATA = array(array());
+
+	    if($result->num_rows == 0) { // so if we have 0 records acc. to keyword display no records found
+	        //echo '<div id="item">Ah snap...! No results found :/</div>';
+	        //$itemSQL = "Ah snap...! No results found : in getPageObjectByID()";
+	        $fieldDATA[0]["status"] = "FAILED";
+			$fieldDATA[0]["info"] = "Ah snap...! No results found : in getObjectsBySearch()";
+	    }
+	    else {
+	         // Get results of query
+	         global $count;
+	         $count = 0;
+	         while($row = $result->fetch_assoc()) {  //outputs the records
+				//$itemSQL = $row['itemSQL'];
+				$fieldDATA[$count]["ID"] = $count;
+	         	$fieldDATA[$count]["SearchTerm"] = $row['SearchTerm'];
+	         	$count++;
+				//echo "IN getObjectsBySearch  ID -- " . $row[$ID] . " itemDisplay -- " . $row[$itemDisplay];
+
+//		        $returnStatus[0]["status"] = "Success";
+//				$returnStatus[0]["info"] = $row['itemSQL'];				
+				//echo "<div > Success -- " . $itemSQL . " ID -- " . $pageObjectID . "</div>";	'"' . $itemSQL . '"'				
+	         }
+		}
+        $result->close();
+        $mysqli->close();
+		return $fieldDATA;
+	}
+
+	function getObjectsBySearch($objectSearch) {
+		// Include config file
+	    require_once 'config.php';
+    
+		$select = "SELECT * ";
+		$from = "FROM `pageObjects` ";
+		$where = "WHERE `SearchTerm` = '" . $objectSearch . "' ";
+		$orderby = "ORDER BY `orderItems` ASC";
+
+		$strSQL = $select . $from . $where . $orderby;
+				
+		//echo $strSQL;
+		
+	    $result = $mysqli->query($strSQL);
+
+	    $fieldDATA = array(array());
+
+	    if($result->num_rows == 0) { // so if we have 0 records acc. to keyword display no records found
+	        //echo '<div id="item">Ah snap...! No results found :/</div>';
+	        //$itemSQL = "Ah snap...! No results found : in getPageObjectByID()";
+	        $fieldDATA[0]["status"] = "FAILED";
+			$fieldDATA[0]["info"] = "Ah snap...! No results found : in getObjectsBySearch()";
+	    }
+	    else {
+	         // Get results of query
+	         global $count;
+	         $count = 0;
+	         while($row = $result->fetch_assoc()) {  //outputs the records
+				$fieldDATA[$count]["ID"] = $row['ID'];
+				$fieldDATA[$count]["SearchTerm"] = $row['SearchTerm'];
+				$fieldDATA[$count]["orderItems"] = $row['orderItems'];
+	         	$fieldDATA[$count]["itemDisplay"] = $row['itemDisplay'];
+				$fieldDATA[$count]["itemSQL"] = $row['itemSQL'];
+				$fieldDATA[$count]["ModifiedDate"] = $row['ModifiedDate'];
+	         	$count++;
+	         }
+		}
+        $result->close();
+        $mysqli->close();
+		return $fieldDATA;
+	}
+	//if($_POST['keyword'] && !empty($_POST['keyword'])){
+	if($_POST['postOBJ'] && !empty($_POST['postOBJ'])){
+		$postOBJ = json_decode($_POST['postOBJ'], TRUE);
+		$forObject = $postOBJ["forObject"];
+		$sqlCommand = $postOBJ["sqlCommand"];		
+		$fieldName = $postOBJ["fieldname"];
+		$clickedData = $postOBJ["clickedData"];
+		$pageObject = $postOBJ["pageObject"];
+
+		switch ($forObject) {
+			case "searchTermDropdown":
+				$returnStatus = getDISTINCTSearchTerms();
+				if($count > 1) {
+					header('Content-type: application/json');
+				}
+				echo json_encode($returnStatus);
+				break;
+			case "searchTermRows":
+				$returnStatus = getObjectsBySearch($pageObject);
+				if($count > 1) {
+					header('Content-type: application/json');
+				}
+				echo json_encode($returnStatus);
+				
+				break;
+			
+		}
+		
+
+	} else {
+		$returnStatus[0]["status"] = "FAILED";
+		$returnStatus[0]["info"] = "No POST data.";
+		//header('Content-type: application/json');	//required if more than 1 row to return!!
+		echo json_encode($returnStatus);		
+	}
+
+?>

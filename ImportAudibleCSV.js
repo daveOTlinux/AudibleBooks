cf1ee28e5aebf19515a90c12c8f0eba92790637d
Dave Mason <dave@gamer.onthebay.info>
2018-08-22 23:27:55 -0300

    Arranged Functions in Alphabetical order.

--- ImportAudibleCSV.js
+++ ImportAudibleCSV.js
@@ -1,4 +1,143 @@
 
+function cleanupPageAfterUpdateInsert(currentImportID) {
+	updateImportRowDone(currentImportID);
+	fillTemplateSpace("titleSpace", "headerImportCSVtemplate", "");
+	fetchImportResults(currentImportID + 1);
+	$('#audibleSpace').empty();
+	fillTemplateSpace("footerSpace", "footerImportCSVtemplate", "");
+	
+}
+
+function displayNoRowsFound() {
+	$('#dialogBoxStatus').modal('hide');
+	fetchImportResults(2);
+	
+}
+
+function fetchAudibleResults() {		// Fills the main Table <div> #maintablebody
+	var inputTitleSearch = $("#inputTitleSearch").val();
+	if (inputTitleSearch.indexOf("'") > -1) {
+		//alert("inputTitleSearch has a " + "' at -" + inputTitleSearch.indexOf("'"));
+		var cleanSearch = inputTitleSearch.slice(0, inputTitleSearch.indexOf("'")) + "''" + inputTitleSearch.slice(inputTitleSearch.indexOf("'") + 1, inputTitleSearch.length);
+	}
+	var $pageSection = $('#audibleSpace');
+	var $templateHTML = $('#bodyAudibletemplate').html();
+	
+	var sqlObject = {
+		"select":"SELECT `ID`, `Title`, `Author`, `Series`, `BookNumber`, `ReadOrderNumber`, `ReadOrder`, `Notes` ",
+		"from":"FROM `AudibleBooks` ",
+		"where":"WHERE `Title` LIKE '%" + cleanSearch + "%'",
+		"order":"",
+		"limits":""
+	};
+	
+	var postData = {
+		"functionCall":"getAudibleRowData",
+		"fieldName":sqlObject,
+		"searchkey":""
+	};
+	var dataString = JSON.stringify(postData);
+	$.ajax({
+		url:'ImportAudibleCSV.php',
+		type:'POST',
+		data: {postOBJ: dataString},
+		success:function(returnData) {
+			//console.log("returnData fetchTableResults -- " + returnData);
+			//alert("In fetchTableResults returnData length -- " + returnData.length);
+			$pageSection.empty();
+			$.each(returnData, function(i, resultitem){
+				if (resultitem.status == 'Success') {
+					var dataMustache = {
+						"modifyAudible-ID":resultitem.ID,
+		  "modifyAudible-Title":resultitem.Title,
+		  "modifyAudible-Author":resultitem.Author,
+		  "modifyAudible-Series":resultitem.Series,
+		  "modifyAudible-BookNumber":resultitem.BookNumber,
+		  "modifyAudible-Button":"updateAudibleTable(this)",
+				   "modifyAudible-ButtonMode":"Update",
+		  "modifyAudible-ReadOrderNumber":resultitem.ReadOrderNumber,
+		  "modifyAudible-ReadOrder":resultitem.ReadOrder,
+		  "modifyAudible-Notes":resultitem.Notes,
+					};
+					$pageSection.append(Mustache.render($templateHTML, dataMustache));
+				}else {
+					//alert("Return from AJAX getAudibleRowData php call. Failed! \n resultitem.info -- " + resultitem.info);
+					var dataMustache = {
+						"modifyAudible-ID":"0",
+		  "modifyAudible-Title":$("#import-Title").val(),
+				   "modifyAudible-Author":$("#import-Author").val(),
+				   "modifyAudible-Series":"",
+		  "modifyAudible-BookNumber":"",
+		  "modifyAudible-Button":"insertAudibleTable(this)",
+				   "modifyAudible-ButtonMode":"Insert",
+		  "modifyAudible-ReadOrderNumber":"",
+		  "modifyAudible-ReadOrder":"",
+		  "modifyAudible-Notes":"",
+					};
+					$pageSection.append(Mustache.render($templateHTML, dataMustache));
+				}
+			});
+		},
+		error: function() {
+			alert('In fetchAudibleResults(). Error with getting row data from AudibleBooks.php.');
+		}
+	});
+}
+
+function fetchImportResults(rowID) {		// Fills the main Table <div> #maintablebody
+	var $pageSection = $('#inputSpace');
+	var $templateHTML = $('#bodyImportCSVtemplate').html();
+	
+	var sqlObject = {
+		"select":"SELECT * ",
+		"from":"FROM `AudibleWeb` ",
+		"where":"WHERE `ID` = " + Number(rowID),
+		"order":"",
+		"limits":""
+	}
+	
+	//	alert("SQL string before call -- \n " + select + "\n" + from + "\n" +
+	//		 where + "\n" + order + "\n" + limits);
+	
+	var postData = {
+		"functionCall":"getImportRowData",
+		"fieldName":sqlObject,
+		"searchkey":""
+	};
+	var dataString = JSON.stringify(postData);
+	$.ajax({
+		url:'ImportAudibleCSV.php',
+		type:'POST',
+		data: {postOBJ: dataString},
+		success:function(returnData) {
+			//console.log("returnData fetchTableResults -- " + returnData);
+			//alert("In fetchTableResults returnData length -- " + returnData.length);
+			$pageSection.empty();
+			$.each(returnData, function(i, resultitem){
+				$("#importHeaderID").text('Import Table Row ID: ' + resultitem.ID);
+				$("#importHeaderID").attr('data-id', resultitem.ID);
+				//alert("");
+				if(resultitem.RowDone == 0) {
+					$("#importHeaderDone").attr('hidden', true);
+				} else {
+					$("#importHeaderDone").attr('hidden', false);
+				}
+				var dataMustache = {
+					"tableRow-ID":resultitem.ID,
+		  "tableRow-Title":resultitem.Title,
+		  "tableRow-Author":resultitem.Author,
+		  "tableRow-Length":resultitem.Length,
+		  "tableRow-strDateAdded":resultitem.strDateAdded,
+		  "tableRow-DateAdded":resultitem.DateAdded,
+				};
+				$pageSection.append(Mustache.render($templateHTML, dataMustache));
+			});
+		},
+		error: function() {
+			alert('In fetchImportResults(). Error with getting row data from ImportAudibleCSV.php.');
+		}
+	});
+}
 
 function fillTemplateSpace(templateDivName, templateName, mustacheData) {
 	var $templateDivSpace = $("#" + templateDivName);
@@ -13,21 +152,18 @@ function getImportRowbyID() {
 	var dialogRowID = parseInt($('#dialogInput-modal').val());
 	//alert("In getImportRowbyID() \n dialogRowID value and type -- " + dialogRowID + " and -- " + typeof(dialogRowID));
 	fetchImportResults(dialogRowID);
-	
 }
 
 function gotoNextRow() {
 	var currentImportID = Number($("#importHeaderID").attr('data-id'));
 	//alert("In gotoNextRow(). currentImportID -- " + currentImportID);
 	fetchImportResults(currentImportID + 1);
-	
 }
 
 function gotoPreviousRow() {
 	var currentImportID = Number($("#importHeaderID").attr('data-id'));
 	//alert("In gotoNextRow(). currentImportID -- " + currentImportID);
 	fetchImportResults(currentImportID - 1);
-	
 }
 
 function gotoRowID() {
@@ -49,32 +185,6 @@ function gotoRowID() {
 	$("#dialogBoxStatus").modal();	//opens modal
 }
 
-function displayNoRowsFound() {
-	$('#dialogBoxStatus').modal('hide');
-	fetchImportResults(2);
-	
-}
-
-function popupDialogModal() {
-	var modalContent = $("#update-modal");
-	var modalTemplate = $('#dialogBox-template').html();
-	
-	modalContent.empty();				
-	var modalData = {
-		"dialogInput-modal":"",
-		"dialogInputType-modal":"hidden",
-		"dialogTitle-modal":"No AudibleBooks Record found.",
-		"dialogBody-modal":"",
-		"acceptButton-modal":"OK",
-		"cancelButton-modal":"",
-		"cancelButton-modalHidden":"hidden",
-		"acceptFunction-modal":"displayNoRowsFound()",
-	};
-	modalContent.append(Mustache.render(modalTemplate, modalData));
-	$("#dialogBoxStatus").modal();	//opens modal
-	
-}
-
 function insertAudibleTable(element) {
 	//var buttonId = $("#" + element.id).attr('id');
 	var currentImportID = Number($("#importHeaderID").attr('data-id'));
@@ -88,16 +198,16 @@ function insertAudibleTable(element) {
 	var inputReadOrderNumber = $('#input-ReadOrderNumber0').val();
 	var inputLength = $('#import-Length').val();
 	var inputDateAdded = $('#import-DateAdded').val();
-/*	alert("In updateAudibleTable(). " +
-	"\n inputTitle -- " + inputTitle +
-	"\n inputAuthor -- " + inputAuthor +
-	"\n inputSeries -- " + inputSeries +
-	"\n inputBookNumber -- " + inputBookNumber +
-	"\n inputReadOrder -- " + inputReadOrder +
-	"\n inputReadOrderNumber -- " + inputReadOrderNumber +
-	"\n inputLength -- " + inputLength +
-	"\n inputDateAdded -- " + inputDateAdded);
-*/
+	/*	alert("In updateAudibleTable(). " +
+	 *	"\n inputTitle -- " + inputTitle +
+	 *	"\n inputAuthor -- " + inputAuthor +
+	 *	"\n inputSeries -- " + inputSeries +
+	 *	"\n inputBookNumber -- " + inputBookNumber +
+	 *	"\n inputReadOrder -- " + inputReadOrder +
+	 *	"\n inputReadOrderNumber -- " + inputReadOrderNumber +
+	 *	"\n inputLength -- " + inputLength +
+	 *	"\n inputDateAdded -- " + inputDateAdded);
+	 */
 	var sqlCommand = "(`ID`, `Title`, `Author`, `Series`, `BookNumber`, `ReadOrderNumber`, " +
 	"`ReadOrder`, `Length`, `DateAdded`, `ModifiedDate`) " +
 	"VALUES (NULL, " +
@@ -109,13 +219,13 @@ function insertAudibleTable(element) {
 	"'" + inputReadOrder + "', " +
 	"'" + inputLength + "', " +
 	"'" + inputDateAdded + "', NOW())";
-
+	
 	var postData = {
 		"functionCall":"insertNewRowTable",
 		"fieldName":sqlCommand,
 		"searchkey":"",		//ID of row to edit
 	};
-
+	
 	var dataString = JSON.stringify(postData);	//convert dataString string to JSON
 	$.ajax({
 		url:'AudibleBooks.php',
@@ -143,6 +253,26 @@ function insertAudibleTable(element) {
 	
 }
 
+function popupDialogModal() {
+	var modalContent = $("#update-modal");
+	var modalTemplate = $('#dialogBox-template').html();
+	
+	modalContent.empty();				
+	var modalData = {
+		"dialogInput-modal":"",
+		"dialogInputType-modal":"hidden",
+		"dialogTitle-modal":"No AudibleBooks Record found.",
+		"dialogBody-modal":"",
+		"acceptButton-modal":"OK",
+		"cancelButton-modal":"",
+		"cancelButton-modalHidden":"hidden",
+		"acceptFunction-modal":"displayNoRowsFound()",
+	};
+	modalContent.append(Mustache.render(modalTemplate, modalData));
+	$("#dialogBoxStatus").modal();	//opens modal
+	
+}
+
 function updateAudibleTable(element) {
 	var buttonId = $("#" + element.id).attr('id');
 	var currentImportID = Number($("#importHeaderID").attr('data-id'));
@@ -160,16 +290,16 @@ function updateAudibleTable(element) {
 	var inputReadOrderNumber = $('#input-ReadOrderNumber' + selectedBookID).val();
 	var inputLength = $('#import-Length').val();
 	var inputDateAdded = $('#import-DateAdded').val();
-/*	alert("In updateAudibleTable(). " +
-	"\n inputTitle -- " + cleanTitle +
-		"\n inputAuthor -- " + inputAuthor +
-		"\n inputSeries -- " + inputSeries +
-		"\n inputBookNumber -- " + inputBookNumber +
-		"\n inputReadOrder -- " + inputReadOrder +
-		"\n inputReadOrderNumber -- " + inputReadOrderNumber +
-		"\n inputLength -- " + inputLength +
-		"\n inputDateAdded -- " + inputDateAdded);
-*/
+	/*	alert("In updateAudibleTable(). " +
+	 * "\n inputTitle -- " + cleanTitle +
+	 *	"\n inputAuthor -- " + inputAuthor +
+	 *	"\n inputSeries -- " + inputSeries +
+	 *	"\n inputBookNumber -- " + inputBookNumber +
+	 *	"\n inputReadOrder -- " + inputReadOrder +
+	 *	"\n inputReadOrderNumber -- " + inputReadOrderNumber +
+	 *	"\n inputLength -- " + inputLength +
+	 *	"\n inputDateAdded -- " + inputDateAdded);
+	 */
 	var sqlCommand = "SET `Title` = '" + cleanTitle +
 	"', `Author` = '" + inputAuthor +
 	"', `Series` = '" + inputSeries +
@@ -216,13 +346,13 @@ function updateAudibleTable(element) {
 function updateImportRowDone(rowID) {
 	var currentImportID = Number($("#importHeaderID").attr('data-id'));
 	var sqlCommand = "SET `RowDone` = 1 ";
-
+	
 	var postData = {
 		"functionCall":"updateImportRowDoneByID",
 		"fieldName":sqlCommand,
 		"searchkey":rowID,
 	};
-
+	
 	var dataString = JSON.stringify(postData);
 	$.ajax({
 		url:'ImportAudibleCSV.php',
@@ -243,140 +373,6 @@ function updateImportRowDone(rowID) {
 		}
 	});
 }
-
-function cleanupPageAfterUpdateInsert(currentImportID) {
-	updateImportRowDone(currentImportID);
-	fillTemplateSpace("titleSpace", "headerImportCSVtemplate", "");
-	fetchImportResults(currentImportID + 1);
-	$('#audibleSpace').empty();
-	fillTemplateSpace("footerSpace", "footerImportCSVtemplate", "");
-	
-}
-
-function fetchAudibleResults() {		// Fills the main Table <div> #maintablebody
-	var inputTitleSearch = $("#inputTitleSearch").val();
-	if (inputTitleSearch.indexOf("'") > -1) {
-		//alert("inputTitleSearch has a " + "' at -" + inputTitleSearch.indexOf("'"));
-		var cleanSearch = inputTitleSearch.slice(0, inputTitleSearch.indexOf("'")) + "''" + inputTitleSearch.slice(inputTitleSearch.indexOf("'") + 1, inputTitleSearch.length);
-	}
-	var $pageSection = $('#audibleSpace');
-	var $templateHTML = $('#bodyAudibletemplate').html();
-	
-	var sqlObject = {
-		"select":"SELECT `ID`, `Title`, `Author`, `Series`, `BookNumber`, `ReadOrderNumber`, `ReadOrder`, `Notes` ",
-		"from":"FROM `AudibleBooks` ",
-		"where":"WHERE `Title` LIKE '%" + cleanSearch + "%'",
-		"order":"",
-		"limits":""
-	};
-
-	var postData = {
-		"functionCall":"getAudibleRowData",
-		"fieldName":sqlObject,
-		"searchkey":""
-	};
-	var dataString = JSON.stringify(postData);
-	$.ajax({
-		url:'ImportAudibleCSV.php',
-		type:'POST',
-		data: {postOBJ: dataString},
-		success:function(returnData) {
-			//console.log("returnData fetchTableResults -- " + returnData);
-			//alert("In fetchTableResults returnData length -- " + returnData.length);
-				$pageSection.empty();
-				$.each(returnData, function(i, resultitem){
-					if (resultitem.status == 'Success') {
-						var dataMustache = {
-							"modifyAudible-ID":resultitem.ID,
-							"modifyAudible-Title":resultitem.Title,
-							"modifyAudible-Author":resultitem.Author,
-							"modifyAudible-Series":resultitem.Series,
-							"modifyAudible-BookNumber":resultitem.BookNumber,
-							"modifyAudible-Button":"updateAudibleTable(this)",
-							"modifyAudible-ButtonMode":"Update",
-							"modifyAudible-ReadOrderNumber":resultitem.ReadOrderNumber,
-							"modifyAudible-ReadOrder":resultitem.ReadOrder,
-							"modifyAudible-Notes":resultitem.Notes,
-						};
-						$pageSection.append(Mustache.render($templateHTML, dataMustache));
-					}else {
-						//alert("Return from AJAX getAudibleRowData php call. Failed! \n resultitem.info -- " + resultitem.info);
-						var dataMustache = {
-							"modifyAudible-ID":"0",
-							"modifyAudible-Title":$("#import-Title").val(),
-							"modifyAudible-Author":$("#import-Author").val(),
-							"modifyAudible-Series":"",
-							"modifyAudible-BookNumber":"",
-							"modifyAudible-Button":"insertAudibleTable(this)",
-							"modifyAudible-ButtonMode":"Insert",
-							"modifyAudible-ReadOrderNumber":"",
-							"modifyAudible-ReadOrder":"",
-							"modifyAudible-Notes":"",
-						};
-						$pageSection.append(Mustache.render($templateHTML, dataMustache));
-					}
-				});
-		},
-		error: function() {
-			alert('In fetchAudibleResults(). Error with getting row data from AudibleBooks.php.');
-		}
-	});
-} 
-
-function fetchImportResults(rowID) {		// Fills the main Table <div> #maintablebody
-	var $pageSection = $('#inputSpace');
-	var $templateHTML = $('#bodyImportCSVtemplate').html();
-	
-	var sqlObject = {
-		"select":"SELECT * ",
-		"from":"FROM `AudibleWeb` ",
-		"where":"WHERE `ID` = " + Number(rowID),
-		"order":"",
-		"limits":""
-	}
-	
-	//	alert("SQL string before call -- \n " + select + "\n" + from + "\n" +
-	//		 where + "\n" + order + "\n" + limits);
-	
-	var postData = {
-		"functionCall":"getImportRowData",
-		"fieldName":sqlObject,
-		"searchkey":""
-	};
-	var dataString = JSON.stringify(postData);
-	$.ajax({
-		url:'ImportAudibleCSV.php',
-		type:'POST',
-		data: {postOBJ: dataString},
-		success:function(returnData) {
-			//console.log("returnData fetchTableResults -- " + returnData);
-			//alert("In fetchTableResults returnData length -- " + returnData.length);
-			$pageSection.empty();
-			$.each(returnData, function(i, resultitem){
-				$("#importHeaderID").text('Import Table Row ID: ' + resultitem.ID);
-				$("#importHeaderID").attr('data-id', resultitem.ID);
-				//alert("");
-				if(resultitem.RowDone == 0) {
-					$("#importHeaderDone").attr('hidden', true);
-				} else {
-					$("#importHeaderDone").attr('hidden', false);
-				}
-				var dataMustache = {
-					"tableRow-ID":resultitem.ID,
-					"tableRow-Title":resultitem.Title,
-					"tableRow-Author":resultitem.Author,
-					"tableRow-Length":resultitem.Length,
-					"tableRow-strDateAdded":resultitem.strDateAdded,
-					"tableRow-DateAdded":resultitem.DateAdded,
-				};
-				$pageSection.append(Mustache.render($templateHTML, dataMustache));
-			});
-		},
-		error: function() {
-			alert('In fetchImportResults(). Error with getting row data from ImportAudibleCSV.php.');
-		}
-	});
-} 
 
 $(document).ready(function(){	//Code to run when page finishes loading
 	fillTemplateSpace("titleSpace", "headerImportCSVtemplate", "");
